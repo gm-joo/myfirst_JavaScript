ef83f11258048f2c0a0daca84f6978060e13c1f1
gm-joo <88270574+gm-joo@users.noreply.github.com>
2021-09-10 11:16:58 +0200

    Add files via upload

---
+++ opdracht_10_JavaScript/index.js
@@ -0,0 +1,17 @@
+console.log("Hello Winc Academy")
+
+let name = 'Gepke'
+console.log(name);
+
+console.log(4 + 4);
+console.log('4' + '4');
+console.log(5 + 12);
+console.log(200 - 50);
+console.log(5 * 7);
+console.log(50 / 5);
+console.log(23 % 5);
+
+let age = 32;
+age = '32';
+console.log(typeof age);
+
