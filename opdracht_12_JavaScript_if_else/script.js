599a250af86051b84372b49dc4d1a9cc7a7164f6
gm-joo <88270574+gm-joo@users.noreply.github.com>
2021-09-19 10:30:55 +0200

    Add files via upload

---
+++ opdracht_12_JavaScript_if_else/script.js
@@ -0,0 +1,26 @@
+const Age = 32;
+
+if (Age < 18) {
+    console.log("je bent jonger dan 18, je mag helaas niet naar binnen");
+} else {
+    console.log("je bent 18 jaar of ouder: welkom in de kroeg en veel plezier!")
+}
+//in je command prompt ga je op zoek naar dit document via je mappen structuur.
+//als je bij het bestand bent gekomen: typ dan: node script.js en ENTER
+//als resultaat krijg je dan de uitkomst van je if else statement
+
+const isFemale = true;
+
+if (isFemale) {
+    console.log("je bent een vrouw");
+} else {
+    console.log("je bent geen vrouw");
+}
+
+const driverStatus = 'geen bob';
+
+if (driverStatus) {
+    console.log("top! je bent de bob dus je mag naar huis rijden");
+} else {
+    console.log("helaas, je bent niet de bob dus je moet een taxi nemen");
+}
