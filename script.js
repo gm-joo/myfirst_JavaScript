6ae64556dd326931725492393c5880a95d54fcea
gm-joo <88270574+gm-joo@users.noreply.github.com>
2021-09-26 12:12:40 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const makePizza = function () {
+    console.log("make the base");
+    console.log("make the sauce");
+    console.log("roll out the dough");
+    console.log("top and bake");
+
+}
+//als je bovenstaande runt, dan krijg je nog geen resultaat omdat je de function geschreven hebt en nog niet heb ge-"called"
+
+//to call the function:
+makePizza();
+
+//now call your function 3 times:
+makePizza();
+makePizza();
+makePizza();
+
+const makeSushi = function () {
+    console.log("make the rice and nori");
+    console.log("spread the rice on the nori");
+    console.log("add fillings");
+    console.log("roll the sushi");
+    console.log("cut the sushi and serve");
+}
+
+//change your code so that the output of your program is the steps for making sushi, three times and pizza two times
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
